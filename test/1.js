0a9dcb415ca7410316c3ac603f9e81fd6c8bd39c
赵利明 <gaoming13@yeah.net>
2021-03-30 16:27:23 +0800

    1

--- test/1.js
+++ test/1.js
@@ -1,2 +1,34 @@
+class A {
+  i = 1;
+  count = 10;
 
+  *[Symbol.iterator]() {
+    for (let j = 0; j < 10; j++) {
+      yield new Promise((resovle) => {
+        setTimeout(() => {
+          resovle(true);
+        }, 1000);
+      });
+    }
+  }
 
+  fly () {
+    console.log('fly');
+  }
+}
+let a = new A();
+
+async function* asyncGenerator() {
+  var i = 0;
+  while (i < 3) {
+    yield i++;
+  }
+}
+
+(async () => {
+  for (num of a) {
+    console.log(await num);
+  }
+})();
+
+console.log('1213');
