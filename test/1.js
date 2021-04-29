5c22d17a4454c5c2b48b1b968860c24abfb8bff4
赵利明 <gaoming13@yeah.net>
2021-04-29 17:23:26 +0800

    1

--- test/1.js
+++ test/1.js
@@ -1,6 +1,26 @@
-document.addEventListener('click', function(e) {
-  console.log(e);
-});
-let e1 = document.createEvent('MouseEvent');
-e1.initMouseEvent('click', true, true, document.defaultView, 0 ,0 ,0 ,0 ,0, false, false, false, false, 0, null);
-document.querySelector('button').dispatchEvent(e1);
+const wokerScriptBlobUrl = URL.createObjectURL(new Blob([`
+self.onmessage = ({data}) => {
+  const view = new Uint32Array(data);
+  for (let i = 0; i < 10000; i++) {
+    view[0] += 1;
+  }
+  console.log(view[0]);
+  self.postMessage(null);
+};
+`]));
+
+const woker1 = new Worker(wokerScriptBlobUrl);
+woker1.onmessage = () => {
+  console.log('worker1 => ' + view[0]);
+};
+const woker2 = new Worker(wokerScriptBlobUrl);
+woker2.onmessage = () => {
+  console.log('worker2 => ' + view[0]);
+};
+
+const sharedArrayBuffer = new SharedArrayBuffer(4);
+const view = new Uint32Array(sharedArrayBuffer);
+view[0] = 0;
+woker1.postMessage(sharedArrayBuffer);
+woker2.postMessage(sharedArrayBuffer);
+
