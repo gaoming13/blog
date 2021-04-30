b143fab2bf1a8496457d77fa06c40700613f873b
赵利明 <gaoming13@yeah.net>
2021-04-30 15:58:00 +0800

    1

--- test/1.js
+++ test/1.js
@@ -1,25 +1,10 @@
-async function* chars() {
-  for (const u8 of [240, 159, 152, 132, 102, 228, 184, 173]) {
-    yield await new Promise((resolve) => setTimeout(resolve, 100, Uint8Array.of(u8)));
+const dragDom = document.querySelector('#drag1');
+function hander(e) {
+  e.preventDefault();
+  if (e.type == 'drop') {
+    dragDom.innerHTML = '<img src="' + window.URL.createObjectURL(e.dataTransfer.files[0]) + '">';
   }
 }
-// 文本流
-const textStream = new ReadableStream({
-  async start(constroller) {
-    for await (const u8 of chars()) {
-      constroller.enqueue(u8);
-    }
-    constroller.close();
-  }
-});
-// 流过管道解码
-const decodeTextStream = textStream.pipeThrough(new TextDecoderStream());
-// 读取
-const reader = decodeTextStream.getReader();
-(async () => {
-  while(true) {
-    const res = await reader.read();
-    if (res.done) break;
-    console.log(res);
-  }
-})();
+dragDom.addEventListener('dragenter', hander);
+dragDom.addEventListener('dragover', hander);
+dragDom.addEventListener('drop', hander);
