33d6987e7a7af7f27bf148119baee9ee81abdfdd
赵利明 <gaoming13@yeah.net>
2021-05-08 15:57:55 +0800

    1

--- 04.JS/模块加载机制/demo-commonjs/lib.js
+++
@@ -1,9 +0,0 @@
-const obj = {
-  num: 0,
-};
-setTimeout(() => {
-  obj.num = 10;
-}, 1000);
-module.exports = obj;
-
-console.log(module);
--- 04.JS/模块加载机制/demo-esm/lib.mjs
+++
@@ -1,5 +0,0 @@
-let count = 0;
-setTimeout(() => {
-  count = 10;
-}, 1000);
-export { count };
